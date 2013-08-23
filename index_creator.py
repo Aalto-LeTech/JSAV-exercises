add40805249aeaa6facbe6f225c0b7d3543563e3
X455u <kassu7@gmail.com>
2013-08-23 09:49:35 +0300

    Improved index creator and changed directory from 0 to a

--- index_creator.py
+++ index_creator.py
@@ -1,8 +1,30 @@
 from glob import glob
 
-files = glob('0/exercises/jsav/*.html')
+files = glob('a/exercises/jsav/*.html')
 
-html = '<html><body>\n<ul>\n'
+html = ('<html>\n'
+	'<head>\n'
+	'<style type="text/css">\n'
+	'  h1, a {\n'
+	'    font-family: Helvetica, arial, freesans, clean, sans-serif;\n'
+	'  }\n'
+	'  a:link, a:visited {\n'
+	'    text-decoration:none;\n'
+	'    color: #03d;\n'
+	'  }\n'
+	'  a:hover, a:active {\n'
+	'    text-decoration:none;\n'
+	'    color: #000;\n'
+	'  }\n'
+	'  ul {\n'
+	'    list-style-type: circle;\n'
+	'  }\n'
+	'</style>\n'
+	'</head>\n'
+	'<body>\n'
+	'<h1>JSAV Exercises</h1>\n'
+	'<ul>\n')
+
 for ex in files:
   html += '<li><a href="'+ex+'">'+ex+'</a></li>\n'
 
