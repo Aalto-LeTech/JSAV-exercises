cb2aff32f595f48a9f06200209fae14c259946d0
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2019-11-15 14:07:05 +0200

    Updating Dijkstra, cherry-picking 1ba62e4e

--- AV/Development/DijkstraPE.js
+++ AV/Development/DijkstraPE.js
@@ -10,7 +10,7 @@
 
   jsav.recorded();
 
-  function init() {
+  function init_old() {
     // create the graph
     if (graph) {
       graph.clear();
@@ -26,6 +26,66 @@
     // mark the 'A' node
     graph.nodes()[0].addClass("marked");
 
+    jsav.displayInit();
+    return graph;
+  }
+
+  function init() {
+    // Settings for input
+    const width = 500, height = 400,  // pixels
+          weighted = true,
+          directed = false,
+          nVertices = 10, nEdges = 12;
+
+    // First create a random planar graph instance in neighbour list format
+    let nlGraph = undefined,
+        bestNlGraph = undefined,
+        bestResult = {score: 0},
+        trials = 0;
+    const targetScore = 5, maxTrials = 100;
+    let sumStats = {
+      relaxations: 0,
+      singleClosest: 0,
+      multipleClosest: 0,
+      longerPath: 0,
+      unreachable: 0 };
+
+    let result = {score: 0};
+    while (result.score < targetScore && trials < maxTrials) {
+      nlGraph = graphUtils.generatePlanarNl(nVertices, nEdges, weighted,
+        directed, width, height);
+      result = testDijkstra(nlGraph);
+      if (result.score > bestResult.score) {
+        bestNlGraph = nlGraph;
+        bestResult = result;
+      }
+      for (let k of Object.keys(result.stats)) {
+        if (result.stats[k] > 0) {
+          sumStats[k]++;
+        }
+      }
+      trials++;
+    }
+    nlGraph = bestNlGraph;
+    let statsText = "Trials: " + trials + "\n";
+    for (let k of Object.keys(sumStats)) {
+      statsText += k + ": " + sumStats[k] + "\n";
+    }
+    console.log(statsText);
+
+    // Create a JSAV graph instance
+    if (graph) {
+      graph.clear();
+    }
+    graph = jsav.ds.graph({//    Condition:
+      width: width,
+      height: height,
+      layout: "manual",
+      directed: directed
+    });
+    graphUtils.nlToJsav(nlGraph, graph);
+    graph.layout();
+    graph.nodes()[0].addClass("marked"); // mark the 'A' node
     jsav.displayInit();
     return graph;
   }
@@ -51,7 +111,7 @@
         graphNodes = graph.nodes();
     // create the model
     var modelGraph = modeljsav.ds.graph({
-      width: 400,
+      width: 500,
       height: 400,
       layout: "automatic",
       directed: false
@@ -175,6 +235,137 @@
     }
   }
 
+
+  function validateInput(graph) {
+    // Checks whether the random graph is a valid exercise input
+    return testDijkstra(graph);
+  }
+
+  /*
+   * Dijkstra's algorithm for a neighbour list format graph and validates
+   * its goodness as an exercise instance.
+   */
+  function testDijkstra(graph) {
+
+    const nNodes = graph.vertices.length;
+    let stats = {
+      relaxations: 0,
+      singleClosest: 0,
+      multipleClosest: 0,
+      longerPath: 0,
+      unreachable: 0 };
+
+    // Initial vertex is at index 0. Array 'distances' stores length of
+    // shortest path from the initial vertex to each other vertex.
+    // Array 'visited' stores the visitedness of each vertex. A visited vertex
+    // has their minimum distance decided permanently.
+    var distance = Array(nNodes);
+    var visited = Array(nNodes);
+    for (let i = 0; i < nNodes; i++) {
+      distance[i] = Infinity;
+      visited[i] = false;
+    }
+    distance[0] = 0;
+
+    for (let i = 0; i < nNodes; i++) {
+      var v = dijkstraMinVertex(distance, visited, stats);
+      visited[v] = true;
+      if (distance[v] === Infinity) {
+        stats.unreachable++;
+        continue;
+      }
+      for (let e of graph.edges[v]) {
+        let d = distance[e.v];
+        let dNew = distance[v] + e.weight;
+        if (dNew < d) {
+          // Update distance
+          if (d < Infinity) {
+            stats.relaxations++;
+          }
+          distance[e.v] = dNew;
+        } else if (visited[e.v] === false) {
+          stats.longerPath++;
+        }
+      }
+    }
+
+    // Analyse statistics
+    let score = 0;
+    // for (let k of Object.keys(stats)) {
+    //   score += (stats[k] > 0) ? 1 : 0;
+    // }
+
+    // Properties of a good Dijkstra input:
+    //
+    // 1. At some point of algorithm, there is a unique choice for the closest
+    //    unvisited vertex.
+    score += (stats.singleClosest > 0) ? 1 : 0;
+
+    // 2. At some point of algorithm, there are multiple equal choices for
+    //    closest unvisited vertex.
+    score += (stats.multipleClosest > 0) ? 1 : 0;
+
+    // 3. There is at least one vertex which is unreachable from the initial
+    //    vertex v0.
+    score += (stats.unreachable > 0) ? 1 : 0;
+
+    // 4. There is a vertex u such that there are at least two different paths,
+    //    p1 and p2, such that both lead from v0 to u, p1 is explored before
+    //    p2, and p2 has lower weight than p1.
+    score += (stats.relaxations > 0) ? 1: 0;
+
+    // 5. There is a vertex u such that there are at least two different paths,
+    //    p1 and p2, such that both lead from v0 to u, p1 is explored before p2,
+    //    and p2 has equal or greater weight than p1.
+    score += (stats.longerPath > 0) ? 1 : 0;
+
+    return {score: score, stats: stats}
+  }
+
+  /*
+   * Helper for testDijkstra(): select nearest unvisited vertex.
+   * If there are multiple nearest vertices, select the one with lowest index.
+   *
+   * Parameters:
+   * distance: array of integers, each having a positive value
+   * visited: array of booleans indicating which vertices are visited
+   * stats: a statistics object created by testDijkstra(). This is updated.
+   *
+   * Returns:
+   * (int): index of the nearest unvisited vertex.
+   */
+  function dijkstraMinVertex(distance, visited, stats) {
+    // Find the unvisited vertex with the smalled distance
+    let v = 0; // Initialize v to first unvisited vertex;
+    for (let i = 0; i < visited.length; i++) {
+      if (visited[i] === false) {
+        v = i;
+        break;
+      }
+    }
+    // Now find the smallest value
+    let equalValues = 1;
+    for (let i = 0; i < visited.length; i++) {
+      if (visited[i] === false) {
+        if (distance[i] < distance[v]) {
+          v = i;
+          equalValues = 1;
+        } else if (distance[i] === distance[v] && i !== v &&
+                   distance[v] < Infinity) {
+          // There are multiple unvisited vertices with the same finite
+          // distance
+          equalValues++;
+        }
+
+      }
+    }
+    if (equalValues === 1) {
+      stats.singleClosest++;
+    } else {
+      stats.multipleClosest++;
+    }
+    return v;
+  }
 
   // Process About button: Pop up a message with an Alert
   function about() {
