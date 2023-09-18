dc92f22beb1e7d71353a97109be65b650894a650
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2023-09-18 17:33:17 +0300

    Scaffolded grader: gradeEnqueueUpdate
    Continuing from previous commit:
    testbench/OpenDSA/AV/Development/ScaffoldedPrimDijkstra.js:
      class PqOperationSequence.gradeEnqueueUpdate():
        new function to handle the matching of subsequent enqueue-update
        operations.

--- AV/Development/ScaffoldedPrimDijkstra.js
+++ AV/Development/ScaffoldedPrimDijkstra.js
@@ -1,32 +1,32 @@
- /**
-   * A class for a priority queue operation.
-   * operation: one of: 'enqueue', 'dequeue', 'update'
-   * edge: a string of two uppercase letters A-Z
-   */
- class PqOperation {
-    constructor(operation, edge) {
-      let valid = true;
-      if (operation !== 'enqueue' && operation !== 'dequeue' && operation !== 'update') {
-        valid = false;
-        console.error('PqOperation: invalid operation: ' + operation);
-      }
-      if (typeof(edge) !== 'string' || /^[A-Z]{2}$/.test(edge) === false) {
-        valid = false;
-        console.error('PqOperation: invalid edge: ' + edge);
-      }
-      if (valid) {
-        this.operation = operation;
-        if (edge[0] <= edge[1]) {
-          this.edge = edge;
-        }
-        else {
-          this.edge = edge[1] + edge[0];
-        }        
-      }
+/**
+ * A class for a priority queue operation.
+ * operation: one of: 'enqueue', 'dequeue', 'update'
+* edge: a string of two uppercase letters A-Z
+  */
+class PqOperation {
+  constructor(operation, edge) {
+    let valid = true;
+    if (operation !== 'enqueue' && operation !== 'dequeue' && operation !== 'update') {
+      valid = false;
+      console.error('PqOperation: invalid operation: ' + operation);
     }
-    equals(x) {
-      return this.operation === x.operation && this.edge === x.edge;
+    if (typeof(edge) !== 'string' || /^[A-Z]{2}$/.test(edge) === false) {
+      valid = false;
+      console.error('PqOperation: invalid edge: ' + edge);
     }
+    if (valid) {
+      this.operation = operation;
+      if (edge[0] <= edge[1]) {
+        this.edge = edge;
+      }
+      else {
+        this.edge = edge[1] + edge[0];
+      }        
+    }
+  }
+  equals(x) {
+    return this.operation === x.operation && this.edge === x.edge;
+  }
 }
 
 /**
@@ -35,82 +35,91 @@
  * answer operations and student's solution.
  */
 class PqOperationSequence {
-    constructor() {
-        this.operations = [];
+  constructor() {
+    this.operations = [];
+  }
+
+  /**
+   * Clears the sequence.
+   * This should be used when the user clicks the Reset button.
+   */
+  clear() {
+    this.operations = [];
+  }
+
+  /**
+   * Removes the last operation from the sequence.
+   * This should be used when the user clicks the Undo button.
+   */
+  undo() {
+    this.operations.pop();
+  }
+
+  /**
+   * Adds new operation to the sequence.
+   * 
+   * @Param {PqOperation} operation
+   */
+  push(operation) {
+    this.operations.push(operation);
+  }
+
+  /**
+   * Union of two Sets.
+   * 
+   * @param {Set} setA 
+   * @param {Set} setB 
+   * @returns 
+   */
+  union(setA, setB) {
+    const _union = new Set(setA);
+    for (const elem of setB) {
+      _union.add(elem);
     }
-    /**
-     * Clears the sequence.
-     * This should be used when the user clicks the Reset button.
-     */
-    clear() {
-        this.operations = [];
-    }
+    return _union;
+  }
 
-    /**
-     * Removes the last operation from the sequence.
-     * This should be used when the user clicks the Undo button.
-     */
-    undo() {
-        this.operations.pop();
-    }
+  /**
+   * 
+   * @param {PqOperation} modelAnswer 
+   */
+  gradeAgainst(modelAnswer) {
+    let studentGrade = 0;
+    const modelLength = modelAnswer.operations.length;
+    
+    // Student's sequence
+    let studSeq = this.operations;
 
-    /**
-     * Adds new operation to the sequence.
-     * 
-     * @Param {PqOperation} operation
-     */
-    push(operation) {
-        this.operations.push(operation);
-    }
-
-    /**
-     * Union of two Sets.
-     * 
-     * @param {Set} setA 
-     * @param {Set} setB 
-     * @returns 
-     */
-    union(setA, setB) {
-        const _union = new Set(setA);
-        for (const elem of setB) {
-          _union.add(elem);
-        }
-        return _union;
-    }
-
-    /**
-     * 
-     * @param {PqOperation} modelAnswer 
-     */
-    gradeAgainst(modelAnswer) {
-        let studentGrade = 0;
-        const maxGrade = modelAnswer.operations.length;
-        let modelSeq = modelAnswer.operations;
-        let studSeq = this.operations;
-        let i = 0;
-        let j = 0;
-        let studentOpSet = new Set();
-        let modelOpSet = new Set();
-
-        while (j < maxGrade) {
-            let modelOp = modelSeq[j];
-            if (modelOp.operation === 'dequeue') {
-                if (studSeq[i].operation === 'dequeue' &&
-                    studSeq[i].edge === modelOp.edge) {
-                        i++;
-                        j++;
-                }
-                else {
-                    studentGrade = i;
-                    break;
-                }
-
-            }
-            else {
-                // TODO
-            }
-        }
+    // Model solution's sequence
+    let modelSeq = modelAnswer.operations;
         
-        return { 'studentGrade': studentGrade, 'maxGrade': maxGrade };
+    let i = 0; // points to studSet
+    let j = 0; // poinst to modelSeq
+
+    while (j < maxGrade) {
+      let modelOp = modelSeq[j];
+      if (modelOp.operation === 'dequeue') {
+        if (studSeq[i].operation === 'dequeue' &&
+            studSeq[i].edge === modelOp.edge) {
+            i++, j++;
+        }
+        else {
+            studentGrade = i;
+            break;
+        }
+      }
+      else {
+        let r = this.gradeEnqueueUpdate(studSeq, modelSeq, i, j);
+        // TODO
+      }
     }
+    
+    return { 'studentGrade': studentGrade, 'maxGrade': modelLength };
+  }
+
+  gradeEnqueueUpdate(studSeq, modelSeq, i, j) {
+    let shouldBreak = false;
+    // TODO
+    return {'i': i, 'j': j, 'break': shouldBreak}
+  }
 }
