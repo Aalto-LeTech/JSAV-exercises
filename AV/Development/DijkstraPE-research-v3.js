ae43ba5a5b8d151bcf60fb04c7ca393fa72aec33
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2023-11-03 13:32:16 +0200

    Scaff.Dijkstra: extract legend code
    Issue #216
    - create file GraphAlgorithms.js
    - move legend creation code from Scaff.Dijkstra to GraphAlgorithms.js
    - test refactored Scaffolded Dijkstra: legend in student & model view should be
      unchanged

--- AV/Development/DijkstraPE-research-v3.js
+++ AV/Development/DijkstraPE-research-v3.js
@@ -852,50 +852,6 @@
   }
 
   /**
-   * Creates a legend box which explains the edge colors.
-   * This is used for both the student's view and the model answer.
-   * 
-   * @param {JSAV} av A JSAV algorithm visualization template
-   * @param {number} x Location: pixels from left in *av*
-   * @param {number} y Location: pixels from top in *av*
-   * @param {function(string)} interpret A JSAV interpreter function
-   */
-  function createLegend(av, x, y, interpret) {
-    // Center on a pixel to produce crisp edges
-    x = Math.floor(x) + 0.5;
-    y = Math.floor(y) + 0.5;
-    const width = 250; // pixels
-    const height = 250; // pixels
-    av.g.rect(x, y, width, height, {
-        "stroke-width": 1,
-        fill: "white",
-    }).addClass("legendbox");
-    av.label(interpret("legend"), {left: x + 100, top: y - 35});
-
-    const hpos = [26, 76, 90]; // line start, line end, text start (pixels)
-    const vpos = [30, 80, 130]; // vertical position for each three edge types
-    const edgeClass = ["legend-edge", "legend-fringe", "legend-spanning"];
-    const edgeText = ["legend_unvisited", "legend_fringe", 
-        "legend_spanning_tree"];
-    const textvadjust = -22;
-    for (let i = 0; i < 3; i++) {
-        av.g.line(x + hpos[0], y + vpos[i],
-                x + hpos[1], y + vpos[i]).addClass(edgeClass[i]);
-        av.label(interpret(edgeText[i]), {left: x + hpos[2],
-                top: y + vpos[i] + textvadjust,
-                "text-align": "center"})
-            .addClass("legendtext")            
-    }
-    av.g.circle(x + 51, y + 201, 22);    
-    av.label("5<br>C (B)", {left: x + 35, top: y + 166})
-        .addClass("legendtext")
-        .addClass("textcentering");
-    av.label(interpret("node_explanation"),
-            {left: x + hpos[2], top: y + 166})
-        .addClass("legendtext");
-  }
-
-  /**
    * Return the parent node of node at index.
    * @param {number} index the index of the node whose parent we want.
    * 
