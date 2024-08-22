fc75edf7047a07020e04374b614e604c42d21309
Teemu Taivainen <136599271+ttaiv@users.noreply.github.com>
2024-08-22 12:22:50 +0300

    Refactor: Extract functions of BFS and DFS to builder class (#265)
    * Move logic for creating BFS exercise functions to builder class
    
    * Utilize exercise builder class in DFS
    
    * Add docstrings for TraversalExerciseBuilder class
    
    * Remove unnecessary variable declarations

--- AV/Development/graphDFSPE.js
+++ AV/Development/graphDFSPE.js
@@ -1,13 +1,11 @@
-/* global ODSA, graphUtils createAdjacencyList*/
-(function($) {
+/* global TraversalExerciseBuilder*/
+(function() {
   "use strict";
   var exercise,
-      graph,
       config = ODSA.UTILS.loadConfig(),
       interpret = config.interpreter,
       settings = config.getSettings(),
-      jsav = new JSAV($(".avcontainer"), {settings: settings}),
-      neighbourList; // type of this is JSAV pseudo code object
+      jsav = new JSAV($(".avcontainer"), {settings: settings});
 
   jsav.recorded();
 
@@ -18,130 +16,6 @@
       .highlight(2);
   } else {
     jsav.code(); // pseudo is just blank if code is not defined
-  }
-
-  function init() {
-    // Settings for input
-    const width = 500, // pixels
-        height = 400,  // pixels
-        weighted = false,
-        directed = false,
-        nVertices = [11, 3],
-        nEdges = [14, 2];
-
-    // First create a random planar graph instance in neighbour list format
-    let nlGraph = graphUtils.generatePlanarNl(nVertices, nEdges, weighted,
-                                              directed, width, height);
-
-    // Assure that the random planar graph has A connected to another node
-    // and a sufficiently large spanning tree, i.e. at least 7 edges
-    while (spanning_tree(nlGraph).length < 7) {
-      console.warn("TOO SMALL SPANNING TREE:", spanning_tree(nlGraph).length);
-      nlGraph = graphUtils.generatePlanarNl(nVertices, nEdges, weighted, directed, width, height);
-    }
-
-    neighbourList?.clear(); // clear neighbour list if it already exist (when reset is clicked)
-    neighbourList = createAdjacencyList(nlGraph, jsav, {
-      lineNumbers: false,
-      after: {element: $(".neighbourlist")}
-    });
-
-    // Create a JSAV graph instance
-    graph?.clear(); // Clear graph on reset
-    graph = jsav.ds.graph({
-      width: width,
-      height: height,
-      layout: "manual",
-      directed: directed
-    });
-    graphUtils.nlToJsav(nlGraph, graph);
-    graph.layout();
-    graph.nodes()[0].addClass("visited"); // mark the 'A' node
-    jsav.displayInit();
-    // Remove the initially calculated size so that the graph sits next
-    // to the code.
-    $(".jsavcanvas").css("min-width", "");
-    return graph;
-  }
-
-  /**
-   * Calculate the spanning tree for the nlGraph. This is used to ensure
-   * that the spanning tree is sufficiently large and the exercise is not
-   * trivially easy.
-   * The spanning tree is calculated using the BFS algorithm.
-   * @param nlGraph as returned by graphUtils.js
-   * @returns spanning tree edge list
-   */
-  function spanning_tree(nlGraph) {
-    let visited = [];
-    let queue = [];
-    let edges = []; //Edges selected in the bfs spanning tree.
-    let node = 0;
-    queue.push(node);
-
-    while (queue.length > 0) {
-      node = queue.shift();
-      visited.push(node);
-      for (const neighbor of nlGraph.edges[node]) {
-        if (!visited.includes(neighbor.v) && !queue.includes(neighbor.v)) {
-          queue.push(neighbor.v);
-          edges.push([node, neighbor.v]);
-        }
-      }
-    }
-    return edges;
-  }
-
-  function fixState(modelGraph) {
-    var graphEdges = graph.edges(),
-        modelEdges = modelGraph.edges();
-
-    // compare the edges between exercise and model
-    for (var i = 0; i < graphEdges.length; i++) {
-      var edge = graphEdges[i],
-          modelEdge = modelEdges[i];
-      if (modelEdge.hasClass("visited") && !edge.hasClass("visited")) {
-        // mark the edge that is marked in the model, but not in the exercise
-        markEdge(edge);
-        break;
-      }
-    }
-  }
-
-  function modelSolution(modeljsav) {
-    var i;
-    // create the model
-    var modelGraph = modeljsav.ds.graph({
-      width: 500,
-      height: 400,
-      layout: "automatic",
-      directed: false
-    });
-
-    // copy the graph and its weights
-    graphUtils.copy(graph, modelGraph, {weights: true});
-    var modelNodes = modelGraph.nodes();
-
-    // Mark the 'A' node
-    modelNodes[0].addClass("visited");
-
-    modeljsav.displayInit();
-
-    // start the algorithm
-    dfs(modelNodes[0], modeljsav);
-
-    modeljsav.umsg(interpret("av_ms_final"));
-    // hide all edges that are not part of the search tree
-    var modelEdges = modelGraph.edges();
-    for (i = 0; i < modelGraph.edges().length; i++) {
-      if (!modelEdges[i].hasClass("visited")) {
-        modelEdges[i].hide();
-      }
-    }
-
-    modeljsav.step();
-
-    return modelGraph;
   }
 
   function markEdge(edge, av) {
@@ -179,11 +53,12 @@
     }
   }
 
+  const builder = new TraversalExerciseBuilder();
 
-  // Process About button: Pop up a message with an Alert
-  function about() {
-    window.alert(ODSA.AV.aboutstring(interpret(".avTitle"), interpret("av_Authors")));
-  }
+  const init = builder.buildInit(jsav);
+  const fixState = builder.buildFixState(exercise);
+  const modelSolution = builder.buildModel(dfs, interpret, false);
+  const aboutAlert = builder.buildAboutAlert(interpret);
 
   exercise = jsav.exercise(modelSolution, init, {
     compare: {class: "visited"},
@@ -204,5 +79,5 @@
     window.alert("Please, click on the edges, not the nodes.");
   });
 
-  $("#about").click(about);
+  $("#about").click(aboutAlert);
 }(jQuery));
