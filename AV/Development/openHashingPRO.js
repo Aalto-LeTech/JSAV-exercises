022041809fe78556faa2c6b3b6de242e2fa08adf
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-26 09:54:29 +0300

    JSAV: Open hashing exercise updated
    https://github.com/Aalto-LeTech/jsav-exercise-recorder/pull/248
    
    - Insert is now performed to the beginning of the list.
    - Clicking the hash table (the linked list head pointer) is required
      and shown.
    - Model answer has separate step for calculating index.
    - Uniqueness of hash table keys is ensured.
    - Adjusted highlighting consistency: each highlight now represents click
      in both UI and model.
    - Some formatting and refactoring.

--- AV/Development/openHashingPRO.js
+++ AV/Development/openHashingPRO.js
@@ -1,6 +1,8 @@
 /* global ODSA, PARAMS */
-(function ($) {
+(function($) {
   "use strict";
+  const KEY_LOWER_BOUND = 100; // inclusive
+  const KEY_UPPER_BOUND = 900; // exclusive
 
   // AV variables
   var hashSize = PARAMS.size || 11,
@@ -13,12 +15,10 @@
       $hashLabel,
       $stackLabel,
       pseudo,
-
       // get the configurations from the configuration file
-      config = ODSA.UTILS.loadConfig({'av_container': 'jsavcontainer'}),
+      config = ODSA.UTILS.loadConfig({av_container: "jsavcontainer"}),
       interpret = config.interpreter,
       code = config.code,
-
       // Create a JSAV instance
       av = new JSAV($("#jsavcontainer"));
 
@@ -101,7 +101,6 @@
         collision: "none",
         within: $(".jsavcanvas")
       });
-      list[i].first().css("opacity", 0);
       list[i].click(clickHandler);
     }
 
@@ -155,31 +154,42 @@
         collision: "none",
         within: $(".jsavcanvas")
       });
-      msList[i].first().css("opacity", 0);
     }
 
     var msClickedIndex = jsav.variable(-1);
 
     jsav.displayInit();
 
-    function find(key) {
+    // Helper function that searches for a key in the linked list,
+    // highlights the nodes that are being checked and registers
+    // the states as gradeable steps. The last step (finding the key
+    // or reaching the end of the list) is not registered as a gradeable step.
+    function findListIdx(key) {
       jsav.umsg(interpret("av_ms_looking_for"), {fill: {
         key: key
       }});
-      var node = msList[key % hashSize].first().next(),
-          i = 1;
-      while (node) {
-        node.highlight();
-        if (node.value() === key) {
-          return i;
+      let currentNode = msList[key % hashSize].first(),
+          currentIdx = 0;
+
+      while (currentNode) {
+        currentNode.highlight();
+
+        if (currentNode.value() === key) {
+          // Found the key.
+          // Actual gradeable step is done when advancing to the next operation.
+          return currentIdx;
         }
-        node = node.next();
-        i++;
-        if (node) {
+        // Move to the next node.
+        currentNode = currentNode.next();
+        currentIdx++;
+        if (currentNode) {
+          // Only register a gradeable step if we are not at the end of the list,
+          // because gradeable step is registered when advancing to the next operation.
           jsav.gradeableStep();
         }
       }
-      return 0;
+      // Did not find the key.
+      return null;
     }
 
     function nextOperation() {
@@ -215,17 +225,24 @@
       ind = firstValue % hashSize;
       msClickedIndex.value(ind);
 
+      jsav.umsg(interpret("av_ms_calculate_index"), {fill: {key: firstValue}});
+      printHashFunction(firstValue);
+      jsav.step();
+
       switch (operation) {
       case "insert":
-        msList[ind].addLast().layout();
-        jsav.effects.moveValue(first, msList[ind].last());
+        // Add a new node after the first sentinel node.
+        // Highlight the sentinel node.
+        msList[ind].first().highlight();
+        msList[ind].add(1).layout();
+        // Move the value from the stack to the new node.
+        jsav.effects.moveValue(first, msList[ind].get(1));
         jsav.umsg(interpret("av_ms_insert"), {fill: {
           index: ind
         }});
-        printHashFunction(firstValue);
         break;
       case "remove":
-        nodeInd = find(firstValue);
+        nodeInd = findListIdx(firstValue);
         if (nodeInd) {
           msList[ind].remove(nodeInd);
           msList[ind].layout();
@@ -237,10 +254,9 @@
             key: firstValue
           }});
         }
-        printHashFunction(firstValue);
         break;
       case "search":
-        nodeInd = find(firstValue);
+        nodeInd = findListIdx(firstValue);
         if (nodeInd) {
           jsav.umsg(interpret("av_ms_search"), {fill: {
             index: ind
@@ -250,10 +266,11 @@
             key: firstValue
           }});
         }
-        printHashFunction(firstValue);
+        break;
+      default:
+        // nothing
       }
       nextOperation();
-
     }
 
     var gradeableModelStructures = msList.slice(0);
@@ -261,100 +278,177 @@
     return gradeableModelStructures;
   }
 
+  // Helper functions to handle the click event on the list nodes.
 
-  var clickHandler = function () {
-    function nextOperation() {
-      unhighlightAllLists();
-      if (opStack.size()) {
+  /**
+   * Process the click event when the top-most element of the stack is an update operation.
+   * @param {Object} clickedNode The node that was clicked.
+   * @param {*} valueToRemove The value that should be removed.
+   * @returns {boolean} True if the operation was finished (i.e., the key was found or the operation was completed),
+   *          false if the operation is still in progress.
+   */
+  function processRemove(clickedNode, valueToRemove) {
+    const nodeList = clickedNode.container;
+    clickedNode.highlight();
+
+    if (clickedNode.value() === valueToRemove) {
+      // User clicked the node, which contains the key
+      // that we want to remove.
+      // -> remove the node
+      for (let idx = 0; idx < nodeList.size(); idx++) {
+        if (nodeList.get(idx) === clickedNode) {
+          nodeList.remove(idx);
+          // Indicate that operation is complete and we should continue with the next operation.
+          return true;
+        }
+      }
+    }
+    if (clickedNode === nodeList.last()) {
+      // The clicked node is the last node in the list.
+      // Indicate that we should continue with the next operation
+      // as the key was not found in the list.
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Process the click event when the top-most element of the stack is a search operation.
+   * @param {Object} clickedNode The node that was clicked.
+   * @param {*} valueToSearch The value that should be searched.
+   * @returns {boolean} True if the operation was finished (i.e., the key was found or the operation was completed),
+   *         false if the operation is still in progress.
+   */
+  function processSearch(clickedNode, valueToSearch) {
+    const nodeList = clickedNode.container;
+    clickedNode.highlight();
+
+    if (clickedNode.value() === valueToSearch) {
+      // Search is complete.
+      return true;
+    }
+    // User clicked some other node in the list.
+    // Check if we are at the end of the list.
+    if (clickedNode === nodeList.last()) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Process the click event when the top-most element of the stack is an insert operation.
+   * NOTE: The return value has different meaning than in the process functions of remove and search.
+   * @param {object} clickedNode The node that was clicked.
+   * @param {object} topElement The top element of the stack.
+   * @returns {boolean} true if the click was valid and insert was performed,
+   *        false if the click was not valid and insert was not performed
+   */
+  function processInsert(clickedNode, topElement) {
+    const clickedList = clickedNode.container;
+    if (clickedNode !== clickedList.first()) {
+      window.alert("Click on the hash table to insert a value.");
+      return false;
+    }
+    clickedNode.highlight();
+    clickedList.add(1); // Add new node after the invisible first node.
+    // Move the value from the stack to the new node.
+    av.effects.moveValue(topElement, clickedList.get(1));
+    return true;
+  }
+
+  const OperationResult = Object.freeze({
+    FINISHED: "finished",
+    IN_PROGRESS: "in_progress",
+    INVALID: "invalid"
+  });
+
+  /**
+   * Apply the operation that is on top of the stack to the clicked node.
+   * @param {object} clickedNode The node that was clicked.
+   * @param {object} topElement The top element of the stack.
+   * @returns {string} One of the OperationResult values.
+   */
+  function applyOperationToClickedNode(clickedNode, topElement) {
+    const operation = getOperationType(topElement);
+    switch (operation) {
+    case "insert": {
+      const validClick = processInsert(clickedNode, topElement, clickedIndex);
+      if (validClick) {
+        // Insert is always finished if the click is valid.
+        return OperationResult.FINISHED;
+      }
+      return OperationResult.INVALID;
+    }
+    case "remove": {
+      const finished = processRemove(clickedNode, topElement.value());
+      if (finished) {
+        return OperationResult.FINISHED;
+      }
+      return OperationResult.IN_PROGRESS;
+    }
+    case "search": {
+      const finished = processSearch(clickedNode, topElement.value());
+      if (finished) {
+        return OperationResult.FINISHED;
+      }
+      return OperationResult.IN_PROGRESS;
+    }
+    default:
+      console.warn("Unknown operation: " + operation);
+      return OperationResult.FINISHED;
+    }
+  }
+
+  // Linked list click handler.
+  function clickHandler() {
+    // Helpers to advance the exercise.
+    function popNonEmptyOperationStack() {
+      if (opStack.size() > 0) {
         opStack.removeFirst();
         opStack.layout();
         showModulusOfKey();
       }
-      exercise.gradeableStep();
     }
-
     function unhighlightAllLists() {
-      for (var i = 0; i < hashSize; i++) {
-        var node = list[i].first();
+      for (let idx = 0; idx < hashSize; idx++) {
+        let node = list[idx].first();
         while (node) {
           node.unhighlight();
           node = node.next();
         }
       }
     }
-
+    // Click handling starts.
+    // this refers to the clicked node if the linked list.
     if (!opStack.size()) {
       return;
     }
-    var first = opStack.first(),
-        operation = getOperationType(first);
-
-    for (var i = 0; i < hashSize; i++) {
-      if (this.container === list[i]) {
+    const clickedList = this.container;
+    // Find the index of the clicked list.
+    for (let i = 0; i < hashSize; i++) {
+      if (clickedList === list[i]) {
         clickedIndex.value(i);
         break;
       }
     }
 
-    switch (operation) {
-    case "insert":
-      this.container.addLast();
-      av.effects.moveValue(first, this.container.last());
-      break;
-    case "remove":
-      if (this.container.size() === 1) {
-        // no visible nodes in the clicked list
-        // -> next operation
-        $.noop();
-      } else if (this.container.first() === this) {
-        // there are nodes in the list
-        // but the user clicked on the invisible first node
-        // -> do nothing
-        return;
-      } else if (this.value() === first.value()) {
-        // the user clicked on node, which contains the key
-        // that we want to remove.
-        // -> remove the node
-        var size = this.container.size();
-        for (i = 0; i < size; i++) {
-          if (this.container.get(i) === this) {
-            this.container.remove(i);
-            break;
-          }
-        }
-      } else {
-        // mark node
-        this.highlight();
-        // continue with the next operation if this is the last node in the list
-        if (this !== this.container.last()) {
-          exercise.gradeableStep();
-          return;
-        }
-      }
-      break;
-    case "search":
-      if (this.container.size() === 1) {
-        // no visible nodes in the clicked list
-        // -> next operation
-        $.noop();
-      } else if (this.container.first() === this) {
-        // there are nodes in the list
-        // but the user clicked on the invisible first node
-        // -> do nothing
-        return;
-      } else if (this.value() === first.value() || this === this.container.last()) {
-        $.noop();
-      } else {
-        this.highlight();
-        exercise.gradeableStep();
-        return;
-      }
-      break;
-    }
-    this.container.layout();
-    nextOperation();
-  };
+    const topElement = opStack.first(); // top element of the stack
+    const operationResult = applyOperationToClickedNode(this, topElement, clickedIndex.value());
 
+    if (operationResult === OperationResult.INVALID) {
+      // Operation was not valid, do not update the state of the exercise.
+      return;
+    }
+    // Operation was valid, update the state of the exercise.
+    clickedList.layout();
+    if (operationResult === OperationResult.FINISHED) {
+      // Operation is finished, continue with the next operation.
+      popNonEmptyOperationStack();
+      unhighlightAllLists();
+    }
+    // Register gradeable step after each valid click.
+    exercise.gradeableStep();
+  }
 
   function getOperationType(node) {
     if (node.hasClass("insert")) {
@@ -363,10 +457,9 @@
       return "remove";
     } else if (node.hasClass("search")) {
       return "search";
-    } else {
-      // unknown operation
-      return undefined;
     }
+    // unknown operation
+    return null;
   }
 
 
@@ -382,12 +475,20 @@
     }});
   }
 
+  function generateNewUniqueValue(currentValues) {
+    let newVal = JSAV.utils.rand.numKey(KEY_LOWER_BOUND, KEY_UPPER_BOUND);
+    // Randomize new value again if it is already in the array.
+    while (currentValues.includes(newVal)) {
+      newVal = JSAV.utils.rand.numKey(KEY_LOWER_BOUND, KEY_UPPER_BOUND);
+    }
+    return newVal;
+  }
 
   // generate hash operations
   function generateHashOperations(size) {
     var values = new Array(size),
         operations = new Array(size),
-        result = { values: values, operations: operations },
+        result = {values: values, operations: operations},
         i, start, end, ind1, ind2, ind3, ind4, colInd;
 
     // the first quarter contains insert operations
@@ -401,14 +502,13 @@
       if (i === ind1 || i === ind2) {
         continue;
       }
-      values[i] = JSAV.utils.rand.numKey(100, 900);
+      values[i] = generateNewUniqueValue(values);
     }
     // collision index
     colInd = values[0] % hashSize;
     // generate colliding values
-    values[ind1] = Math.floor(JSAV.utils.rand.numKey(100, 900) / hashSize) * hashSize + colInd;
-    values[ind2] = Math.floor(JSAV.utils.rand.numKey(100, 900) / hashSize) * hashSize + colInd;
-
+    values[ind1] = Math.floor(generateNewUniqueValue(values) / hashSize) * hashSize + colInd;
+    values[ind2] = Math.floor(generateNewUniqueValue(values) / hashSize) * hashSize + colInd;
 
     // the second quarter contains remove operations
     // attempt to remove non-existing key at ind1
@@ -425,17 +525,16 @@
       values[i] = values[Math.floor(Math.random() * start)];
     }
     // new key
-    values[ind1] = JSAV.utils.rand.numKey(100, 900);
+    values[ind1] = generateNewUniqueValue(values);
     // already removed key
-    values[ind2] = values[(ind1 === start ? start + 1: start)];
-
+    values[ind2] = values[(ind1 === start ? start + 1 : start)];
 
     //the third quarter contains more insert operations
     start = end;
     end += Math.floor(size / 4);
     for (i = start; i < end; i++) {
       operations[i] = "insert";
-      values[i] = JSAV.utils.rand.numKey(100, 900);
+      values[i] = generateNewUniqueValue(values);
     }
 
 
@@ -471,5 +570,4 @@
     modelDialog: {width: 760}
   });
   exercise.reset();
-
 }(jQuery));
